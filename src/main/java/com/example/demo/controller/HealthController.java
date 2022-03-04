6b63e7b4a1bb569f0d6e7f5660fd248dad7135e4
utente-corso-06 <utente.corso.06@addvalue.it>
2022-03-04 16:41:33 +0100

    + fix health controller

--- src/main/java/com/example/demo/controller/HealthController.java
+++ src/main/java/com/example/demo/controller/HealthController.java
@@ -5,6 +5,7 @@ import com.example.demo.domain.dto.UserLoginDTO;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,12 +15,12 @@ import java.util.List;
 public class HealthController {
 
     @RequestMapping(path="/ready", method = RequestMethod.GET)
-    public ResponseEntity<void> isReady() {
-        return ResponseEntity.ok(void);
+    public ResponseEntity<Void> isReady() {
+        return ResponseEntity.ok(null);
     }
 
     @RequestMapping(path="/health", method = RequestMethod.GET)
-    public List<UserDTO> findAll() {
-        return ResponseEntity.ok(void);
+    public ResponseEntity<Void> findAll() {
+        return ResponseEntity.ok(null);
     }
 }
